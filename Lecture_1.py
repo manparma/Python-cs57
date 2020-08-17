0b44238d89b3831bc6958209ec36e486d3bd2478
Manish Parmar <manishparmar@Manishs-MacBook-Pro.local>
2020-08-17 07:35:52 -0700

    added comments

--- Lecture_1.py
+++ Lecture_1.py
@@ -1,16 +1,24 @@
-#
+# defining variable name
 x = 5
-y = 10
-print (y*x)
+y = 9
 
+# multiply both variables and print
+print (y*x)  # 45
+
+# <type 'int'>
+print(type(4))
+print(type(x))
+print(type(y))
+print(type(x/y))
 
 print(1+1)      # 2
 print(1+1.0)    # 2.0
-print(1.0+1.0)
-print(2*2)
-print(2*2.0)
-print(3/2)
-print(3/2.0)
-print(3/2.0)
-print((2+2)*3.0)
-print (1+1,1+1.0,1.0+1.0,2*2,2*.0,3/2,3/2.0,(2+2)*3.0)
+print(1.0+1.0)  # 2.0
+print(2*2)      # 4
+print(2*2.0)    # 4.0
+print(3/2)      # 1
+print(3/2.0)    # 1.5
+print(type(3/2.0))    # 1.5 <type 'float'>
+print(type((2+2)*3.0))    # 12.0 <type 'float'>
+print (1+1,1+1.0,1.0+1.0,2*2,2*.0,3/2,3/2.0,(2+2)*3.0)  # (2, 2.0, 2.0, 4, 0.0, 1, 1.5, 12.0)
+
