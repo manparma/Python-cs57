590fd385fa04270e0a552ea7b6c3e79ec6254cdb
Manish Parmar <manishparmar@Manishs-MacBook-Pro.local>
2020-08-17 07:44:10 -0700

    added Homework_1

---
+++ Homework_1.py
@@ -0,0 +1,3 @@
+# Homework assignments
+
+#  1. Evaluate ((True and False) and True) or (True and False) or (True or True)
--- Lecture_1.py
+++ Lecture_1.py
@@ -1,11 +1,11 @@
-# defining variable name
+#   defining variable name
 x = 5
 y = 9
 
-# multiply both variables and print
+#   multiply both variables and print
 print (y*x)  # 45
 
-# <type 'int'>
+#   <type 'int'>
 print(type(4))
 print(type(x))
 print(type(y))
@@ -21,4 +21,19 @@ print(3/2.0)    # 1.5
 print(type(3/2.0))    # 1.5 <type 'float'>
 print(type((2+2)*3.0))    # 12.0 <type 'float'>
 print (1+1,1+1.0,1.0+1.0,2*2,2*.0,3/2,3/2.0,(2+2)*3.0)  # (2, 2.0, 2.0, 4, 0.0, 1, 1.5, 12.0)
+
+
+#   Introduction to bools
+5 > 4   # True
+5 >= 4  # True
+3 < 3   # False
+3 <= 3  # True
+2 == 2.0    # True
+
+
+
+
+
+
+
 
