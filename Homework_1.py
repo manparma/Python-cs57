eacee9c58e8bae7ada3adece011345c2701d9ecb
manparma <parmar.c.manish@gmail.com>
2020-08-25 07:30:29 -0700

    Homework_1 Completed

--- Homework_1.py
+++ Homework_1.py
@@ -14,10 +14,19 @@ print(a[2:])  # tsanddogs
 print(a[-3])  # o
 
 #  3. Asks a user to enter a word (via raw_input) and outputs the length of the word.
+name = raw_input("My name is: ")
+print ("Your name is", name, " and contains ", len(name), " letters.")
+# print(len(name)) # another way
 
 
 # 4. Asks a user to enter a number (via raw_input) and outputs the cube of that number.
 # a. For example, input 5 would result in output of 125
+number = (int(raw_input('Enter a number: ')))   # use int to specify for Integers types
+print(number ** 3)
 
 
 # 5. Asks a user to enter a radius (via raw_input) and outputs the area of a circle with the associated radius.
+radius = (float(raw_input('Enter a radius: ')))  # use float to specify for types
+pi = 3.14159
+area = (pi * radius * radius)
+print(area)
